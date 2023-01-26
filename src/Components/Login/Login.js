c4d7c73191bfa6e47b006090d2282f3c00efc255
jaqueuchoab <jaqueline.uchoab@gmail.com>
2023-01-26 19:38:23 -0300

    Configurando Rotas Iniciais

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {Routes, Route} from  'react-router-dom';
+import LoginForm from './LoginForm.js';
+import LoginCreate from './LoginCreate.js';
+import LoginPasswordLost from './LoginPasswordLost.js';
+import LoginPasswordReset from './LoginPasswordReset.js';
+
+
 
 const Login = () => {
   return (
     <div>
-      Login
+      <Routes>
+        <Route path='/' element={<LoginForm />}/>
+        <Route path='/create' element={<LoginCreate />}/>
+        <Route path='/lost' element={<LoginPasswordLost />}/>
+        <Route path='/reset' element={<LoginPasswordReset />}/>
+      </Routes>
     </div>
   )
 }
