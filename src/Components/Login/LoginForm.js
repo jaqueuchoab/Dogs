53f0c436bb5f14ea7f328d023992af673e2c7cf9
jaqueuchoab <jaqueline.uchoab@gmail.com>
2023-02-08 15:22:05 -0300

    Configurando API
    Criação de um arquivo js com funções que auxiliarão no maneijo da API

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -3,27 +3,24 @@ import {Link} from  'react-router-dom';
 import Input from '../Forms/Input.js';
 import Button from '../Forms/Button.js';
 import useForm from '../../Hooks/useForm.js';
+import { TOKEN_POST } from '../../api.js';
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
     if(username.validate && password.validate) {
-      fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(),
-      }).then((response) => {
-        console.log(response);
-        return response.json();
-      }).then((json) => {
-        console.log(json);
-        return json;
+      const { url, options } = TOKEN_POST({
+        username: username.value, 
+        password: password.value,
       });
+
+      const response = await fetch(url, options);
+      const json = await response.json();
+
+      console.log(json)
     }
   }
 
