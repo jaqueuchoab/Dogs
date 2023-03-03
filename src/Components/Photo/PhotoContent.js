ec2a4b9a4e73e326cbe0654e9eb6322a308d2e1c
jaqueuchoab <jaqueline.uchoab@gmail.com>
2023-03-03 15:00:51 -0300

    Feed Modal - Parte 1

---
+++ src/Components/Photo/PhotoContent.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import style from '../../Styles/StylesPhoto/PhotoContent.module.css';
+import PhotoComments from './PhotoComments';
+
+const PhotoContent = ({data}) => {
+  const { photo, comments } = data;
+
+  return (
+    <div className={style.photo}>
+      <div className={style.img}>
+        <img src={photo.src} alt={photo.title} />
+      </div>
+      <div className={style.details}>
+        <div>
+          <p>
+            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            <span className={style.visualizacoes}>{photo.acessos}</span>
+          </p>
+          <h1 className='title'>
+            <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
+          </h1>
+          <ul>
+            <li>{photo.peso} kg</li>
+            <li>{photo.idade === 1 ? `${photo.idade} ano` :  `${photo.idade} anos`}</li>
+          </ul>
+        </div>
+      </div>
+      <PhotoComments id={photo.id} comments={comments} />
+    </div>
+  )
+}
+
+export default PhotoContent;
