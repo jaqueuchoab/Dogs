434fd4cfe08f4548cfbc7dbe83459aec906b7d26
jaqueuchoab <jaqueline.uchoab@gmail.com>
2023-05-02 16:15:13 -0300

    Criando PhotoCommentForm

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { UserContext } from '../../UserContext.js'
+import PhotoCommentsForm from './PhotoCommentsForm';
 
-const PhotoComments = ({comments}) => {
+const PhotoComments = ({props}) => {
+  // Só terá comentários caso algum usário esteja logado, para veriicação usa o contexto abaixo:
+  const { login } = React.useContext(UserContext);
   return (
     <div>
-      
+      {login && <PhotoCommentsForm id={props.id}/>}
     </div>
   )
 }
