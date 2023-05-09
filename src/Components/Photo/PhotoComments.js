70987fdc8cecf0ac9548454721e2b8bfba36284c
jaqueuchoab <jaqueline.uchoab@gmail.com>
2023-05-09 16:04:18 -0300

    Concertando barra de scrll em comentários

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -14,7 +14,7 @@ const PhotoComments = (props) => {
   }, [comments])
 
   return (
-    <div>
+    <>
       <ul ref={commentsSection} className={style.comments}>
         {comments.map(comment => <li key={comment.comment_ID}>
           <b>{`${comment.comment_author}: `}</b>
@@ -22,7 +22,7 @@ const PhotoComments = (props) => {
         </li>)}
       </ul>
       {login && <PhotoCommentsForm id={props.id} setComments={setComments}/>}
-    </div>
+    </>
   )
 }
 
