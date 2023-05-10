9e4b681ee249d18a85ecb2f3e8dbbee86f6d2bae
jaqueuchoab <jaqueline.uchoab@gmail.com>
2023-05-10 15:32:51 -0300

    Photo delete desenvolvido
    Implementando a opção de deletar foto

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import style from '../../Styles/StylesPhoto/PhotoContent.module.css';
+import { UserContext } from '../../UserContext';
 import PhotoComments from './PhotoComments';
+import PhotoDelete from './PhotoDelete';
 
 const PhotoContent = ({data}) => {
+  const user = React.useContext(UserContext);
   const { photo, comments } = data;
 
   return (
@@ -14,21 +17,27 @@ const PhotoContent = ({data}) => {
       <div className={style.details}>
         <div>
           <p className={style.author}>
-            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            )}
             <span className={style.visualizacoes}>{photo.acessos}</span>
           </p>
-          <h1 className='title'>
+          <h1 className="title">
             <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
           </h1>
           <ul className={style.attributes}>
             <li>{photo.peso} kg</li>
-            <li>{photo.idade === 1 ? `${photo.idade} ano` :  `${photo.idade} anos`}</li>
+            <li>
+              {photo.idade === 1 ? `${photo.idade} ano` : `${photo.idade} anos`}
+            </li>
           </ul>
         </div>
         <PhotoComments id={photo.id} comments={comments} />
       </div>
     </div>
-  )
+  );
 }
 
 export default PhotoContent;
