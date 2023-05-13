66dc6fa611f7fff6f2be8df6defd7862859292da
jaqueuchoab <jaqueline.uchoab@gmail.com>
2023-05-13 15:43:00 -0300

    Image Skeleton implementado

--- src/Components/Feed/FeedPhotosItem.js
+++ src/Components/Feed/FeedPhotosItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import style from '../../Styles/StylesFeed/FeedPhotosItem.module.css';
+import Image from '../Helper/Image';
 
 const FeedPhotosItem = ({photo, setModalPhoto}) => {
   function handleClick() {
@@ -8,7 +9,7 @@ const FeedPhotosItem = ({photo, setModalPhoto}) => {
 
   return (
     <li className={style.photo} onClick={handleClick}>
-      <img src={photo.src} title={photo.title} alt={photo.title} />
+        <Image src={photo.src} title={photo.title} alt={photo.title}/>
       <span className={style.visualizacao}>{photo.acessos}</span>
     </li>
   )
