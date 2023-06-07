0fd3349a09cd801d4c1f26d2dd40280962cd66e8
jaqueuchoab <jaqueline.uchoab@gmail.com>
2023-06-07 15:26:14 -0300

    Bug resolvido

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -4,13 +4,15 @@ import {Routes, Route} from  'react-router-dom';
 import Feed from '../Feed/Feed';
 import UserPhotoPost from './UserPhotoPost';
 import UserStatis from './UserStatis';
+import { UserContext } from '../../UserContext';
 
 const User = () => {
+  const {data} = React.useContext(UserContext);
   return (
     <section className='container'>
       <UserHeader />
       <Routes>
-        <Route path='/' element={<Feed />}/>
+        <Route path='/' element={<Feed user={data.id}/>}/>
         <Route path='post' element={<UserPhotoPost />}/>
         <Route path='statistics' element={<UserStatis />}/>
       </Routes>
