92c1edfacb38fd859d239ee533dc89f755d06326
jaqueuchoab <jaqueline.uchoab@gmail.com>
2023-07-12 20:34:37 -0300

    Password Lost e Reset

--- src/api.js
+++ src/api.js
@@ -80,16 +80,6 @@ export function PHOTO_GET(id) {
   };
 }
 
-/*export function PHOTO_GET(id) {
-  return{
-    url: `${API_URL}/api/photo/${id}`,
-    options: {
-      method: 'GET',
-      cache: 'no-store',
-    },
-  };
-}*/
-
 export function COMMENT_POST(id, body) {
   return{
     url: `${API_URL}/api/comment/${id}`,
@@ -112,6 +102,32 @@ export function PHOTO_DELETE(id) {
       headers: {
         Authorization: 'Bearer ' + window.localStorage.getItem('token'),
       }
+    }
+  }
+}
+
+export function PASSWORD_LOST(body) {
+  return {
+    url: API_URL + '/api/password/lost',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type':'application/json',
+      },
+      body: JSON.stringify(body),
+    }
+  }
+}
+
+export function PASSWORD_RESET(body) {
+  return {
+    url: API_URL + '/api/password/reset',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type':'application/json',
+      },
+      body: JSON.stringify(body),
     }
   }
 }
