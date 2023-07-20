1b30094d8f07b1e2298fe098891bfca94d91e6e3
jaqueuchoab <jaqueline.uchoab@gmail.com>
2023-07-19 22:33:13 -0300

    statis in progress

--- src/Components/User/UserStatis.js
+++ src/Components/User/UserStatis.js
@@ -1,7 +1,24 @@
 import React from 'react'
 import Head from '../Helper/Head';
+import useFetch from '../../Hooks/useFetch';
+import { GET_STATS } from '../../api';
 
 const UserStatis = () => {
+  const {data, error, loading, request} = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const {url, options} = GET_STATS();
+      await request(url, options);
+    }
+  }, [request]);
+
+  if(loading) <Loaging />
+  if(error) <Error />
+  if(data) {
+    
+  }
+
   return (
     <div>
       <Head title='Estatisticas' />
